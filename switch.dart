c9a40eb886fd0b56c80abf5ae0ef13aee0be8273
أسامه الشرعبي <120444119+Osama-alharabi@users.noreply.github.com>
2023-02-06 23:43:22 +0300

    Add files via upload

---
+++ switch.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main() {
+
+  var a="osama";
+  switch (a) {
+    case'a':
+      print("1000");
+      break;
+    case'osama':
+    print("*********************");
+    print("Osama amin al-sharabi");
+    print("*********************");
+    break;
+    default:
+    print("**************");
+    
+  }
+}
+
